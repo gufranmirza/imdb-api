2bad1612d6912ee011d26ee1a6acffbe3da21313
Gufran Mirza <gufranmirza1@gmail.com>
2020-11-22 03:47:21 +0530

    Add data loader

--- src/main.go
+++ src/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/gufranmirza/imdb-api/dal/moviedal"
+	"github.com/gufranmirza/imdb-api/database/dbmodels"
 
 	"github.com/gufranmirza/imdb-api/config"
 	"github.com/gufranmirza/imdb-api/database/migrations"
@@ -37,19 +42,69 @@ func main() {
 
 	// Run migration if present in args
 	if len(os.Args) > 1 && os.Args[1] == "migrate" {
-		if len(os.Args) < 3 {
-			log.Fatalf("No enough arguments to run migrations args: (%s) \n", os.Args)
-		}
-		action := os.Args[2]
-		message := ""
-		if len(os.Args) > 3 {
-			message = os.Args[2]
-		}
-		migrations.Migrate(action, message)
+		migrate(os.Args)
+		return
+	}
+
+	// load data from json file
+	if len(os.Args) > 1 && os.Args[1] == "dataloader" {
+		dataloader(os.Args)
 		return
 	}
 
 	// Start http server
 	svr := server.NewServer()
 	svr.Start()
+}
+
+func migrate(args []string) {
+	if len(os.Args) < 3 {
+		log.Printf("No enough arguments to run migrations args: (%s) \n", os.Args)
+		log.Println("USAGE ./imdb-api migrate up/down/new `message here`")
+		os.Exit(1)
+	}
+
+	action := os.Args[2]
+	message := ""
+	if len(os.Args) > 3 {
+		message = os.Args[2]
+	}
+	migrations.Migrate(action, message)
+}
+
+func dataloader(args []string) {
+	if len(os.Args) < 3 {
+		log.Printf("No enough arguments to dataloader args: (%s) \n", os.Args)
+		log.Println("USAGE ./imdb-api migrate dataloader ./data.json")
+		os.Exit(1)
+	}
+
+	path := args[2]
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Failed to read file %s with error: %v \n", path, err)
+	}
+	defer file.Close()
+
+	buff, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Failed to read file %s with error: %v \n", path, err)
+	}
+
+	// we initialize our Users array
+	var movies []dbmodels.Movie
+	moviedal := moviedal.NewMovieDal()
+	err = json.Unmarshal(buff, &movies)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal with error: %v \n", err)
+	}
+
+	for _, movie := range movies {
+		err = moviedal.Create("", &movie)
+		if err != nil {
+			log.Printf("Failed to insert movie with error: %v \n", err)
+			continue
+		}
+	}
 }
